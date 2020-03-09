9d8345a206ea06f4a5c8833f9aa36b1c5cff51b9
[Shinn Krammer] <[aa9e2d379a5e29cc15ac5aa7953e8ca0c32ac299]>
2020-03-10 10:59:05 +1300

    Start of analysis of booleens, =, == and ===.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-//
+// 1.0 Arithmetic Operators
+/* 
+Operator    Description
+    +       Addition
+    -       Subtraction
+    *       Multiply
+    /       Divide
+    %       Modulus
+    **      Power
+*/
+
+// Assigning (=) is to assign a variable
+// Equivalent (==) is the comparison check of different values
+// Identical / Strict equality (===) is the comparison of datatype (Datatype check)
+
+console.log(1 == "1"); // true - Because the num 1 is EQUIVALENT to the string of 1. Values within strings can be equivalent. 
+console.log(1 === "1"); // false - Because the num 1 is not IDENTICAL to the string of 1
+console.log(" ");
+
+console.log(1 == true); // true - True as a Booleen is equivalent to the value of 1
+console.log(1 === true); // false - True is not identical to the value of 1
+console.log(" ");
+
+console.log(0 == false); // true - False as a Booleen is equivalent to 0
+console.log(0 === false); // false - false is not identical to the value of 0
+console.log(" ")
+
+
+// null
+console.log(null + 1);
+console.log(undefined > 0);
+console.log(undefined + 1);
